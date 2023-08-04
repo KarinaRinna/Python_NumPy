1ebf1919d04c305210ed512306269603ad783796
Mandarin <zurual@mail.ru>
2023-08-04 14:15:58 +0300

    1

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-# This is a sample Python script.
+import numpy
+import matplotlib
+import numpy as np
+
+weights_input_to_hidden = np.random.uniform(-0.5, 0.5, (20, 784))
+weights_input_to_output = np.random.uniform(-0.5, 0.5, (10, 20))
+
+bias_input_to_hidden = np.zeros((20, 1))
+bias_input_to_output = np.zeros((10, 1))
 
